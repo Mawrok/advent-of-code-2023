d11e0a96bc80ddcb19f73e1f4f96a17f208c604d
Mawrok <60617904+Mawrok@users.noreply.github.com>
2023-12-01 17:56:59 +0100

    Update day_01.cpp

--- day_01.cpp
+++ day_01.cpp
@@ -1,32 +1,23 @@
-#include <iostream>
-#include <algorithm>
-#include <ranges>
-#include <vector>
-#include <string>
+import std;
 #define PART2
 
-auto calibrate(std::string str) {
-    static std::vector<std::string> strdigs = {
+auto calibration_value(std::string str) {
+    static const std::vector<std::string> strdigs = {
         "1", "2", "3", "4", "5", "6", "7", "8", "9"
 #ifdef PART2
         , "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
 #endif
     };
 
-    using idx_val = std::pair<std::size_t, int>;
-    idx_val first { std::string::npos, 0 };
-    idx_val last  { 1, 0 };
+    std::pair<std::size_t, int> first{ std::string::npos, 0 }, last{ 1, 0 };
     for (const auto [i, strdig] : std::views::enumerate(strdigs)) {
-        int value = 1 + i % 9;
-        first = std::min({     str. find(strdig), value }, first);
-        last  = std::max({ 1 + str.rfind(strdig), value }, last );
+        first = std::min({     str. find(strdig), 1 + i % 9 }, first );
+        last =  std::max({ 1 + str.rfind(strdig), 1 + i % 9 }, last  );
     }
-
     return first.second * 10 + last.second;
 }
 
 int main() {
-    auto calibration_values = std::views::istream<std::string>(std::cin)
-        | std::views::transform(calibrate);
-    std::cout << std::ranges::fold_left(calibration_values, 0, std::plus{});
+    std::cout << std::ranges::fold_left(std::views::istream<std::string>(std::cin)
+        | std::views::transform(calibrate), 0, std::plus{});
 }
