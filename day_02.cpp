ad2ea7d542a311f597a958d6a5055df2fd27d394
Mawrok <60617904+Mawrok@users.noreply.github.com>
2023-12-02 15:57:00 +0100

    Update day_02.cpp

--- day_02.cpp
+++ day_02.cpp
@@ -1,24 +1,15 @@
-#include <iostream>
-#include <string>
-#include <map>
-#include <sstream>
-#include <algorithm>
+import std;
 
 int main() {
-    int part1{}, part2{};
-    int game{1};
-    for (std::string line; std::getline(std::cin, line); ++game) {
-        std::istringstream iss(line); 
-        iss.ignore(7, ':');
-        int value;
-        std::string color;
+    int part1{}, part2{}, value, game{1};
+    for (std::string line, color; std::getline(std::cin, line); ++game) {
         std::map<char, int> counter;
-        while (iss >> value >> color) {
+        std::istringstream iss(line); 
+        for (iss.ignore(7, ':'); iss >> value >> color;) {
             counter[color.front()] = std::max(counter[color.front()], value);
         }
 
-        bool valid = counter['r'] <= 12 and counter['g'] <= 13 and counter['b'] <= 14;
-        part1 += valid ? game : 0;
+        part1 += counter['r'] <= 12 and counter['g'] <= 13 and counter['b'] <= 14 ? game : 0;
         part2 += counter['r'] * counter['g'] * counter['b'];
     }
 
